235d066414bda847467fed95cb8faa0da62b5752
Richard Yang <richyang510@gmail.com>
2021-01-16 11:15:37 -0500

    Improvements to audio manipulation lib

--- backend/audio_manip.py
+++ backend/audio_manip.py
@@ -15,6 +15,8 @@ def dump_wav(filename):
     logging.info(f"Converting {filename} to mp3")
     AudioSegment.from_wav(f"{filename}.wav").export(f"{filename}.mp3", format="mp3")
 
+    return f"{filename}.mp3"
+
 
 # changes tempo of "{filename}.wav" from old_bpm to new_bpm, saves to "{filename}_{new_npm}.wav"
 def change_tempo(filename, old_bpm, new_bpm):
@@ -30,11 +32,16 @@ def change_tempo(filename, old_bpm, new_bpm):
     logging.info(f"Saving {new_filename}.wav")
     sf.write(f"{new_filename}.wav", y_stretched, sr, format='wav')
 
+    return new_filename
+
 
 # changes the key of "{filename}.wav" from old_key to new_key, saves to "{filename}_{new_key}.wav"
-def change_key(filename, old_key, new_key, old_key_minor=False):
+def change_key(filename, old_key, new_key, old_key_minor=False, new_key_minor=False):
     if old_key_minor:
         old_key = minor_to_major_pitch_class(old_key)
+
+    if new_key_minor:
+        new_key = minor_to_major_pitch_class(new_key)
 
     # first determine if it is closer to shift upwards or downwards
     up_transpose = ((new_key if new_key >= old_key else new_key + 12) - old_key) % 12
@@ -56,7 +63,15 @@ def change_key(filename, old_key, new_key, old_key_minor=False):
     logging.info(f"Saving file {filename}_{new_key}.wav")
     sf.write(f"{filename}_{new_key}.wav", y_shift, sr, format='wav')
 
+    return f"{filename}_{new_key}"
 
-change_tempo("Smooth", 112, 133)
-change_key("Smooth_133", 9, 5, True)
-dump_wav("Smooth_133_5")
+
+# Example usage:
+#   Santana - Smooth is in key of Am (9, minor=True) with 112 bpm
+#   to transform to key of F (7, minor=False) with bpm 133:
+#   transform("Smooth", 112, 113, 9, 7, old_key_minor=True)
+def transform(filename, old_bpm, new_bpm, old_key, new_key, old_key_minor=False, new_key_minor=False):
+    f_tempo = change_tempo(filename, old_bpm, new_bpm)
+    f_tempo_key = change_key(f_tempo, old_key, new_key, old_key_minor, new_key_minor)
+    dump_wav(f_tempo_key)
+
