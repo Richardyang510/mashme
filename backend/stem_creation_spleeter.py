87df24214f68e7fc2622c4761f26a38950d04c5c
Cliff Chen <c424chen@uwaterloo.ca>
2021-01-16 23:49:59 -0800

    spleeter update with comments

--- backend/stem_creation_spleeter.py
+++ backend/stem_creation_spleeter.py
@@ -1,8 +1,11 @@
 from spleeter.separator import Separator
-import time
+
+# in_path - path of input song(s): song.mp3
+# out_path - path of output stems: vocal.wav, bass.wav, drums.wav, others.wav
+# runtime is varied and long, ranges from 35-40 seconds
 
 
 def split_song(in_path, out_path):
-    separator = Separator("spleeter:4stems")
+    separator = Separator("spleeter:4stems") # specifies the number of stems produced
 
     return separator.separate_to_file(in_path, out_path)
