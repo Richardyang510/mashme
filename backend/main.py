b1729c8d8f6855144245368da7886502a9319802
Bill Zhang <bill.nonbusiness@gmail.com>
2021-01-17 09:07:26 -0500

    expand mix api

--- backend/main.py
+++ backend/main.py
@@ -13,6 +13,46 @@ BUCKET_NAME = "dropdowns-stems"
 
 app = Flask(__name__)
 
+# song_dst will be transformed to match song_src key and tempo
+def transform_song(song_src_yt_id, song_dst_yt_id):
+    _, _, src_track, src_artist, src_tempo, src_key, src_is_minor = mysql_helper.fetch_song(song_src_yt_id)
+    _, _, dst_track, dst_artist, dst_tempo, dst_key, dst_is_minor = mysql_helper.fetch_song(song_dst_yt_id)
+
+    # fetch source stems
+    src_stems_exist, src_stems = mysql_helper.fetch_stems(song_src_yt_id, src_tempo, src_key, src_is_minor)
+
+    if not src_stems_exist:
+        # this is a big problem lmao F
+        pass
+
+    # check if dst stems exist
+    dst_stems_exist, dst_stems = mysql_helper.fetch_stems(song_dst_yt_id, src_tempo, src_key, src_is_minor)
+
+    if not dst_stems_exist:
+        # fetch original stems
+        logging.info("Could not find transformed stems, making it")
+        _, dst_stems = mysql_helper.fetch_stems(song_dst_yt_id, dst_tempo, dst_key, dst_is_minor)
+
+        for _, stem_type, bucket_name, file_name, stem_key, stem_tempo, _ in dst_stems:
+            file_path = file_name.split(".")[0]
+            transformed_stem = \
+                audio_manip.transform(file_path, stem_tempo, src_tempo, stem_key, src_key, new_key_minor=src_is_minor)
+            gcp_storage_helper.upload_blob(BUCKET_NAME, transformed_stem, transformed_stem)
+            gcp_storage_helper.post_upload(song_dst_yt_id, bucket_name, stem_type, transformed_stem, transformed_stem,
+                                           src_tempo, src_key, 0)
+
+    logging.info("Fetching transformed stems")
+    _, dst_stems = mysql_helper.fetch_stems(song_dst_yt_id, src_tempo, src_key, src_is_minor)
+    output = []
+
+    for _, stem_type, bucket_name, file_name, _, _, _ in src_stems:
+        output.append((stem_type, bucket_name, file_name))
+
+    for _, stem_type, bucket_name, file_name, _, _, _ in dst_stems:
+        output.append((stem_type, bucket_name, file_name))
+
+    return output
+
 
 @app.route('/')
 def hello():
@@ -31,7 +71,7 @@ def smooth_test():
 def mix(query):
     print("The user has entered: " + query)
     queries = query.split(';')
-
+    yids = [] # youtube ids
     for q in queries:
         yt_metadata = ytdl_helper.download(q.replace(',', ' '))
 
@@ -42,6 +82,7 @@ def mix(query):
             pass
 
         youtube_id = yt_metadata["id"]
+        yids.append(youtube_id)
 
         spotify_id = SpotifyHelper.getSongId(yt_metadata["track"], yt_metadata["artist"])
         spotify_features = SpotifyHelper.getAudioFeaturesByIds([spotify_id])
@@ -60,6 +101,9 @@ def mix(query):
 
         only_files = [f for f in listdir(stems_path) if isfile(join(stems_path, f))]
 
+        if is_minor:
+            song_key = audio_manip.minor_to_major_pitch_class(song_key)
+
         for file in only_files:
             stem_type = file.split(".")[0]
             mp3_file = audio_manip.dump_wav(file)
@@ -67,7 +111,9 @@ def mix(query):
             gcp_storage_helper.upload_blob(BUCKET_NAME, file_path, file_path)
             gcp_storage_helper.post_upload(youtube_id, BUCKET_NAME, stem_type, file_path, file_path, tempo, song_key, yt_metadata["duration"])
 
-    return json.dumps([])
+    stems_info = transform_song(yids)
+
+    return json.dumps(stem_type)
 
 
 @app.route('/cached-songs')
