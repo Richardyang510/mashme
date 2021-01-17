2d8fd9851fd7f3c948f5118018d6275283a7bc8f
Mo Abidi <70994925+mo-abidi@users.noreply.github.com>
2021-01-17 03:01:55 -0500

    Final commit before I begin messing around with array thing

--- frontend/personalwebsite.js
+++ frontend/personalwebsite.js
@@ -1,50 +1,3 @@
-/*
-
-document.querySelectorAll(".floppy-all").forEach((link) =>{
-  link.addEventListener("mouseover", () =>{
-    console.log("mouse is over link");
-      /*3px border */
-/*
-      if (link.classList.contains("floppy-projects")) {
-          document.querySelector("#dittoMac").src = "pngs/macintosh128khardhat.png"
-      }
-      else if (link.classList.contains("floppy-about")) {
-          document.querySelector("#dittoMac").src = "pngs/macintosh128keyes.png"
-      }
-      else if (link.classList.contains("floppy-resume")) {
-          document.querySelector("#dittoMac").src = "pngs/macintosh128kresume.png"
-      }
-  })
-})
-
-document.querySelectorAll(".floppy-all").forEach((link) =>{
-  link.addEventListener("mouseout", () =>{
-    console.log("mouse is over link");
-    document.querySelector("#dittoMac").src = "pngs/macintosh128kditto.png"
-  })
-})
-
-console.log("this is working");
-
-function mouseoverImage() {
-    document.getElementById("dittoMac").src ="pngs/macintosh128kspotify.png"
-}
-
-function mouseoutImage() {
-  document.getElementById("dittoMac").src ="pngs/macintosh128kditto.png"
-}
-
-
-random code i made to check if event listener could be added to smth else
-window.onload=function(){
-var item = document.getElementById("dittoMac");
-  item.addEventListener("mouseover", func, false);
-function func() {
-  item.src="pngs/macintosh128kspotify.png"
-  }
-}
-*/
-
 //document.getElementById("two-songs")
 
 function toggleSearchRow() {
@@ -160,9 +113,9 @@ VolumeSample.finishedLoading = function(bufferList) {
   if (!context.createGain) {
     context.createGain = context.createGainNode;
   }
-  
+
   var sources = [null, null, null, null, null, null, null, null];
-  
+
   for (var i = 0; i < num_tracks; i++) {
 	  gainNodes[i] = context.createGain();
 	  sources[i] = context.createBufferSource();
