92aae993e54f96f3e8d230a3fa290eae312996c0
Bill Zhang <bill.nonbusiness@gmail.com>
2021-01-16 20:23:50 -0500

    create test endpoint for smooth

--- backend/main.py
+++ backend/main.py
@@ -7,3 +7,10 @@ app = Flask(__name__)
 def hello():
     name = request.args.get("name", "World")
     return f'Hello, {escape(name)}!'
+
+@app.route('/smooth-test')
+def smooth_test():
+    urls = ['http://34.117.53.148/Smooth_133_5.mp3', 'http://34.117.53.148/Smooth_100_11.mp3']
+    return ','.join(urls)
+
+app.run()
